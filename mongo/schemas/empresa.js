79c34993b4ef2f6c3a2b525fb09f7daef1791c25
Richar <ripupo88@gmail.com>
2020-07-07 04:47:48 +0100

    xx

--- mongo/schemas/empresa.js
+++ mongo/schemas/empresa.js
@@ -4,13 +4,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const empresa_schema = new mongoose.Schema({
-   nombre: String,
-   cif: String,
-   admin: {
-      type: Schema.ObjectId,
-      ref: "usuario"
-   },
-   chat: { type: Number, unique: true }
+    nombre: String,
+    cif: String,
+    admin: {
+        type: Schema.ObjectId,
+        ref: "usuario",
+    },
+    HorarioSemana: [{
+      lunes: {
+         turnos: Number,
+         horarios:[{
+            nombre: String,
+            
+         }]
+      }
+    }],
+    chat: { type: Number, unique: true },
 });
 
 module.exports = mongoose.model("empresa", empresa_schema);
+
+HorarioSemana: [
+    {
+        lunes: {
+            Turnos: 3,
+            horarios: [
+                {
+                    nombre: "M",
+                    horaEntrada: "5:30",
+                    horaSalida: "13:30",
+                },
+                {
+                    nombre: "T",
+                    horaEntrada: "13:30",
+                    horaSalida: "21:30",
+                },
+                {
+                    nombre: "N",
+                    horaEntrada: "21:30",
+                    horaSalida: "5:30",
+                },
+            ],
+        },
+    },
+];
